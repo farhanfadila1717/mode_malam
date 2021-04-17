55534ea189f50f91a503c6d9c180a4331e9abb3e
Farhan Fadila <43161050+farhanfadila1717@users.noreply.github.com>
2021-04-17 19:52:22 +0700

    Fix Typo

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,17 +3,11 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme(BuildContext context) {
   return ThemeData.light().copyWith(
     scaffoldBackgroundColor: Colors.lightBlueAccent[50],
-    textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.black,
-        ),
   );
 }
 
 ThemeData darkTheme(BuildContext context) {
-  return ThemeData.light().copyWith(
+  return ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Colors.blueGrey[900],
-    textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.white,
-        ),
   );
 }
